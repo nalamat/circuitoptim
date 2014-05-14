68335118addb87b5f250f62e4f9fdf5990d0432a
Nima Alamatsaz <nnalamat@gmail.com>
2014-05-14 19:00:10 +0430

    Created optim.m

---
+++ optim.m
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                       %
+% This file is a part of the CircuitOptim project:                      %
+% A circuit optimization toolbox for MATLAB based on SPICE simulations  %
+% Copyright (C) 2014, Nima Alamatsaz, All rights reserved               %
+% Email: nnalamat@gmail.com                                             %
+% Web: http://www.github.com/nalamat/circuitoptim                       % 
+%                                                                       %
+% CircuitOptim is free software: you can redistribute it and/or modify  %
+% it under the terms of the GNU General Public License as published by  %
+% the Free Software Foundation, either version 3 of the License, or     %
+% any later version.                                                    %
+%                                                                       %
+% CircuitOptim is distributed in the hope that it will be useful,       %
+% but WITHOUT ANY WARRANTY; without even the implied warranty of        %
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          %
+% GNU General Public License for more details.                          %
+%                                                                       %
+% You should have received a copy of the GNU General Public License     %
+% along with CircuitOptim. If not, see <http://www.gnu.org/licenses/>.  %
+%                                                                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function optim()
+	
+end
