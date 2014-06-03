c69d7ca82ea3d0f01d8e1ecc09d53f0d4685a71f
Nima Alamatsaz <nnalamat@gmail.com>
2014-06-04 02:05:56 +0430

    Convert spice multipliers

--- val2double.m
+++ val2double.m
@@ -22,5 +22,29 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [vals] = val2double(strs)
+	multis(1 ) = struct('symbol', 'meg', 'value', 'e6'  );
+	multis(2 ) = struct('symbol', 'a'  , 'value', 'e-18');
+	multis(3 ) = struct('symbol', 'f'  , 'value', 'e-15');
+	multis(4 ) = struct('symbol', 'p'  , 'value', 'e-12');
+	multis(5 ) = struct('symbol', 'n'  , 'value', 'e-9' );
+	multis(6 ) = struct('symbol', 'u'  , 'value', 'e-6' );
+	multis(7 ) = struct('symbol', 'm'  , 'value', 'e-3' );
+	multis(8 ) = struct('symbol', 'k'  , 'value', 'e3'  );
+	multis(9 ) = struct('symbol', 'x'  , 'value', 'e6'  );
+	multis(10) = struct('symbol', 'g'  , 'value', 'e9'  );
+	multis(11) = struct('symbol', 't'  , 'value', 'e12' );
+	
+	if (iscell(strs))
+		for i=1:length(strs)
+			for j=1:length(multis)
+				strs{i} = regexprep(strs{i}, multis(j).symbol, multis(j).value, 'ignorecase');
+			end
+		end
+	else
+		for j=1:length(multis)
+			strs = regexprep(strs, multis(j).symbol, multis(j).value, 'ignorecase');
+		end
+	end
+
 	vals = str2double(strs);
 end
