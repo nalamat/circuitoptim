940916b70d77e27986235c1bb9c885e9bcbbd8e5
Nima Alamatsaz <nnalamat@gmail.com>
2014-06-04 01:07:00 +0430

    Fix val2double function rename

--- read_sp.m
+++ read_sp.m
@@ -39,15 +39,15 @@ function [ content params options ] = read_sp( netlist_path )
 		for i=1:c
 			option = regexp(line_options{i}, '[-+.\da-zA-Z]*', 'match');
 			if (length(option) < 2); continue; end
-			lower = val2num(option{1});
-			upper = val2num(option{2});
+			lower = val2double(option{1});
+			upper = val2double(option{2});
 			if (isempty(lower) || isempty(upper)); continue; end
 			scale = 'dec';
 			if (length(option) > 2 && strcmp(lower(option{3}), 'lin'))
 				scale = 'lin';
 			end
 			
-			params  = horzcat(params, val2num(line_params{i}));
+			params  = horzcat(params, val2double(line_params{i}));
 			options = horzcat(options, struct('lower',lower, 'upper',upper, 'scale',scale));
 		end
 		
